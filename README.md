26fbf59fe8c4682a037c77307a8f1de1cdce0d66
Abdullah <hak.99.abdullah.0@gmail.com>
2023-10-01 23:35:20 +0300

    exercise 3.7

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# 3.1: Phonebook backend step 1
+# 3.7: Phonebook backend step 7
 
-In part0 there is a statement like this:
+Morgan middleware added. Morgan is an HTTP request logger middleware for Node.js. It simplifies logging of incoming requests. Here it is used with a 'tiny' configuration, which allows us to log minimal request information.
 
-A system for detecting plagiarism is used to check exercises submitted to GitHub. If code is found from model answers or multiple students hand in the same code, the situation is handled according to the policy on plagiarism of the University of Helsinki.
-
-Therefore, my desire to write original code causes me to go a little beyond what is required in the exercises. In 3.1 I skipped the request in Exercise 1 to display the requested contact list in json format on the server page. However, there was also concern about not being able to pass the task. That's why I wanted to send this as an extra.
+Thus, logging capabilities have been added to the Express application.
--- index.js
+++ index.js
@@ -1,41 +1,94 @@
-const http = require('http')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
 
+app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
     {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
     {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
     {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
-]
+];
 
-
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    let responseText = "[\n"
+app.get('/api/persons', (request, response) => {
+    let html = '<table border="1"><tr><th>ID</th><th>Name</th><th>Number</th></tr>';
     persons.forEach(person => {
-        responseText += "  - " + JSON.stringify(person, null, 2) + ",\n"
-    })
-    responseText = responseText.slice(0, -2) + "\n]"
-    response.end(responseText)
-})
+        html += `<tr><td>${person.id}</td><td>${person.name}</td><td>${person.number}</td></tr>`;
+    });
+    html += '</table>';
+    response.send(html);
+});
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+});
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        });
+    }
+
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        });
+    }
+
+    const id = Math.floor(Math.random() * 10000);
+
+    const person = {
+        id: id,
+        name: body.name,
+        number: body.number
+    };
+
+    persons = persons.concat(person);
+
+    response.json(person);
+});
+
+app.get('/info', (request, response) => {
+    const date = new Date();
+    response.send(`Phonebook has info for ${persons.length} people<br/>${date}`);
+});
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
