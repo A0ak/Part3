fb2e914c1b2fbd261dc1e283baf9390955103580
Abdullah <59019111+A0ak@users.noreply.github.com>
2023-10-09 13:39:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -80,10 +80,9 @@ app.get('/info', (request, response) => {
 });
 
 
-app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001
