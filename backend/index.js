da59923671281e3ec7a1b75f3b6edd144cba8158
Abdullah <hak.99.abdullah.0@gmail.com>
2023-10-13 19:21:21 +0300

    exercise3.11

---
+++ backend/index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const morgan = require('morgan')
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
+const app = express()
+app.use(function(req, res, next) {
+    res.setHeader("Content-Security-Policy", "default-src 'none'; style-src 'self' https://www.gstatic.com;");
+    return next();
+  });
+
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(express.json()) 
+
+morgan.token('post', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
+
+let persons = JSON.parse(fs.readFileSync('./db.json')).persons;
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).json({ error: 'Person not found' })
+    }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const initialLength = persons.length
+    persons = persons.filter(person => person.id !== id)
+
+    if (persons.length < initialLength) {
+        fs.writeFileSync('./db.json', JSON.stringify({ persons: persons }));
+        response.status(204).end()
+    } else {
+        response.status(404).json({ error: 'Person not found' })
+    }
+});
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        });
+    }
+
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        });
+    }
+
+    const person = {
+        id: uuidv4(),
+        name: body.name,
+        number: body.number
+    };
+
+    persons = persons.concat(person);
+
+    fs.writeFileSync('db.json', JSON.stringify({ persons: persons }));
+
+    response.json(person);
+});
+
+app.get('/info', (request, response) => {
+    const date = new Date();
+    response.send(`Phonebook has info for ${persons.length} people<br/>${date}`);
+});
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
