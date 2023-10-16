63bfe4c1f35d380c83d0426f3016be441c8ec6e5
Abdullah <59019111+A0ak@users.noreply.github.com>
2023-10-16 22:25:36 +0300

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://Abdullah:${456852}@cluster0.mongodb.net/phonebook?retryWrites=true&w=majority`
+const url = `mongodb+srv://Abdullah:${*******}@cluster0.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
